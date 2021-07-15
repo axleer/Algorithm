88c4ab3085481a302aedaf1fab31716ee9bb694f
aoxianglee <aoxianglee@aliyun.com>
2021-07-15 21:03:40 +0800

    Update: leet  Improve: format

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ My English is not very good, so there will be many expression may confuse you.
 
   I will marked each problem id in leetcode and provided a link for each of them.
   Also I will recommend a solution with higher performance to you.
+  The problem with hard tag will ignore and func name will be change to write benchmark test.
 
 #### Update status
 
--- leet/problem22.go
+++ leet/problem22.go
@@ -11,46 +11,48 @@ package leet
 // Then every well-formed parentheses will have a closed '()'
 // When you remove it you will find it's well-formed parentheses contains n-1 pairs
 // So the solution can be recursion.
-// func generateParenthesis(n int) []string {
-// 	// handle error input
-// 	if n < 1 {
-// 		return nil
-// 	}
-// 	// init recursion, also the recursion condition
-// 	if n == 1 {
-// 		return []string{"()"}
-// 	}
-// 	// recursion call
-// 	previous := generateParenthesis(n - 1)
-// 	results := []string{}
-// 	for i := 0; i < len(previous); i++ {
-// 		// handle every n-1 results
-// 		for j := 0; j < len(previous[i])+1; j++ {
-// 			result := previous[i][:j] + "()" + previous[i][j:]
-// 			// try to insert the result if not exist
-// 			exist := false
-// 			for k := 0; k < len(results); k++ {
-// 				if result == results[k] {
-// 					exist = true
-// 					break
-// 				}
-// 			}
-// 			if !exist {
-// 				results = append(results, result)
-// 			}
-// 		}
-// 	}
-// 	return results
-// }
+
+func GenerateParenthesis1(n int) []string {
+	// handle error input
+	if n < 1 {
+		return nil
+	}
+	// init recursion, also the recursion condition
+	if n == 1 {
+		return []string{"()"}
+	}
+	// recursion call
+	previous := GenerateParenthesis1(n - 1)
+	results := []string{}
+	for i := 0; i < len(previous); i++ {
+		// handle every n-1 results
+		for j := 0; j < len(previous[i])+1; j++ {
+			result := previous[i][:j] + "()" + previous[i][j:]
+			// try to insert the result if not exist
+			exist := false
+			for k := 0; k < len(results); k++ {
+				if result == results[k] {
+					exist = true
+					break
+				}
+			}
+			if !exist {
+				results = append(results, result)
+			}
+		}
+	}
+	return results
+}
 
 // ---Solution 2---
 
 // This solution was based on leftParenthesesNum >= rightParenthesesNum
 // Also use the recursion, but need a helper function
 // global result avoid an argument pass in helper, so imporve performance
+
 var result []string
 
-func generateParenthesis(n int) []string {
+func GenerateParenthesis2(n int) []string {
 	// Set to empty, and let GC to clean the previous memory
 	result = nil
 	helper(n, 0, nil)
